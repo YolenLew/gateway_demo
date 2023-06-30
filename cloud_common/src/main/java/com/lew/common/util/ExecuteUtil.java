63be998c8b8f157b1752d9e24367f065ff2ae0ae
yolenlew <yolenlew@163.com>
2023-06-30 22:36:24 +0800

    feat: 隔离测试类配置文件

--- cloud_common/src/main/java/com/lew/common/util/ExecuteUtil.java
+++ cloud_common/src/main/java/com/lew/common/util/ExecuteUtil.java
@@ -21,8 +21,7 @@ import java.util.function.Function;
 import java.util.stream.Collectors;
 
 /**
- * 分批调用接口工具
- * 原文链接：https://blog.csdn.net/w605283073/article/details/101399427
+ * 分批调用接口工具 原文链接：https://blog.csdn.net/w605283073/article/details/101399427
  *
  * @author Yolen
  * @date 2022/9/18
@@ -32,9 +31,9 @@ public class ExecuteUtil {
      * 执行分批调用无返回值的消费函数
      *
      * @param dataList 数据源
-     * @param size     每批次执行数据大小
+     * @param size 每批次执行数据大小
      * @param consumer 消费型函数
-     * @param <T>      泛型
+     * @param <T> 泛型
      */
     public static <T> void partitionRun(List<T> dataList, int size, Consumer<List<T>> consumer) {
         if (CollectionUtils.isEmpty(dataList)) {
@@ -48,10 +47,10 @@ public class ExecuteUtil {
      * 执行分批调用带返回值的消费函数
      *
      * @param dataList 数据源
-     * @param size     每批次执行数据大小
+     * @param size 每批次执行数据大小
      * @param function 方法型函数
-     * @param <T>      入参集合泛型
-     * @param <V>      返参集合泛型
+     * @param <T> 入参集合泛型
+     * @param <V> 返参集合泛型
      * @return 分批执行结果
      */
     public static <T, V> List<V> partitionCall2List(List<T> dataList, int size, Function<List<T>, List<V>> function) {
@@ -71,12 +70,12 @@ public class ExecuteUtil {
     /**
      * 异步执行分批调用带返回值的消费函数
      *
-     * @param dataList        数据源
-     * @param size            每批次执行数据大小
-     * @param function        方法型函数
+     * @param dataList 数据源
+     * @param size 每批次执行数据大小
+     * @param function 方法型函数
      * @param executorService 线程池
-     * @param <T>             入参集合泛型
-     * @param <V>             返参集合泛型
+     * @param <T> 入参集合泛型
+     * @param <V> 返参集合泛型
      * @return 分批执行结果
      */
     public static <T, V> List<V> partitionCall2ListAsync(List<T> dataList, int size, ExecutorService executorService,
@@ -121,8 +120,8 @@ public class ExecuteUtil {
         }
         Preconditions.checkArgument(size > 0, "size must not be a minus");
 
-        return Lists.partition(dataList, size).stream().map(function).filter(Objects::nonNull)
-            .reduce(Maps.newHashMap(), (resultMap1, resultMap2) -> {
+        return Lists.partition(dataList, size).stream().map(function).filter(Objects::nonNull).reduce(Maps.newHashMap(),
+            (resultMap1, resultMap2) -> {
                 resultMap1.putAll(resultMap2);
                 return resultMap1;
             });
--- cloud_portal/src/test/java/com/lew/gateway/dao/StudentDaoTest.java
+++ cloud_portal/src/test/java/com/lew/gateway/dao/StudentDaoTest.java
@@ -13,6 +13,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.LinkedList;
@@ -31,6 +32,7 @@ import java.util.stream.Stream;
  */
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@TestPropertySource(locations = "classpath:config/application.yml")
 @Slf4j
 public class StudentDaoTest {
     @Autowired
