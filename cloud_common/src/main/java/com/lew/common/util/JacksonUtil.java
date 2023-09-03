dacfccf2b4e115f0285d92785b4c25473af7186e
yolenlew <yolenlew@163.com>
2023-09-03 19:52:29 +0800

    feat: JacksonUtil工具类补充泛型及Map转换场景

--- cloud_common/src/main/java/com/lew/common/util/JacksonUtil.java
+++ cloud_common/src/main/java/com/lew/common/util/JacksonUtil.java
@@ -14,8 +14,16 @@ import com.fasterxml.jackson.databind.SerializationFeature;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
 
-import java.io.IOException;
+import java.lang.reflect.ParameterizedType;
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.Objects;
+import java.util.concurrent.Callable;
 
 /**
  * @author Yolen
@@ -37,20 +45,24 @@ public class JacksonUtil {
             .configure(SerializationFeature.FAIL_ON_EMPTY_BEANS, false);
     }
 
+    // -------------------------------------------------------------------------------------
+    // ----------------------             【实体类相关】                  ----------------------
+    // -------------------------------------------------------------------------------------
+
     /**
      * 对象转Json格式字符串 参考：<a href="https://www.cnblogs.com/christopherchan/p/11071098.html">利用Jackson封装常用JsonUtil工具类</a>
      *
      * @param obj 对象
      * @return Json格式字符串
      */
-    public static <T> String obj2String(T obj) {
+    public static <T> String obj2Str(T obj) {
         if (Objects.isNull(obj)) {
             return null;
         }
         try {
             return obj instanceof String ? (String)obj : MAPPER.writeValueAsString(obj);
         } catch (JsonProcessingException e) {
-            log.warn("failed to parse Object to String : {}", e.getMessage());
+            log.error("failed to parse Object to String : {}", e.getMessage(), e);
             return null;
         }
     }
@@ -58,40 +70,250 @@ public class JacksonUtil {
     /**
      * 字符串转换为自定义对象
      *
-     * @param str 要转换的字符串
+     * @param json  要转换的字符串
      * @param clazz 自定义对象的class对象
      * @return 自定义对象
      */
-    public static <T> T string2Obj(String str, Class<T> clazz) {
-        if (StringUtils.isBlank(str) || Objects.isNull(clazz)) {
+    public static <T> T str2Obj(String json, Class<T> clazz) {
+        if (StringUtils.isBlank(json) || Objects.isNull(clazz)) {
             return null;
         }
         try {
-            return clazz.equals(String.class) ? (T)str : MAPPER.readValue(str, clazz);
+            return clazz.equals(String.class) ? (T)json : MAPPER.readValue(json, clazz);
         } catch (Exception e) {
-            log.warn("Parse String to Object error : {}", e.getMessage());
+            log.error("Parse String to Object error : {}", e.getMessage(), e);
             return null;
         }
     }
 
-    public static <T> T string2Obj(String str, TypeReference<T> typeReference) {
-        if (StringUtils.isEmpty(str) || typeReference == null) {
+    public static <T> T str2Obj(String json, TypeReference<T> typeReference) {
+        if (StringUtils.isBlank(json) || Objects.isNull(typeReference)) {
             return null;
         }
+
         try {
-            return (T)(typeReference.getType().equals(String.class) ? str : MAPPER.readValue(str, typeReference));
-        } catch (IOException e) {
-            log.warn("Parse String to Object error", e);
+            return (T)(typeReference.getType().equals(String.class) ? json : MAPPER.readValue(json, typeReference));
+        } catch (Exception e) {
+            log.error("Parse String to Object error : {}", e.getMessage(), e);
             return null;
         }
     }
 
-    public static <T> T str2Collection(String str, Class<?> collectionClazz, Class<?>... elementClazzes) {
-        JavaType javaType = MAPPER.getTypeFactory().constructParametricType(collectionClazz, elementClazzes);
+    /**
+     * 反序列化复杂嵌套泛型的实体类(比如CommonResult<List<User>>这种复杂的嵌套实体类)
+     *
+     * @param json             json
+     * @param parameterClasses 内层嵌套泛型参数
+     * @param <T>              返回值泛型
+     * @return 嵌套泛型的实体类
+     */
+    public static <T> T str2GenericObj(String json, Class<?>... parameterClasses) {
         try {
-            return MAPPER.readValue(str, javaType);
-        } catch (IOException e) {
-            log.warn("Parse String to Object error : {}" + e.getMessage());
+            JavaType javaType = constructParameterizedType(null, parameterClasses);
+            return MAPPER.readValue(json, javaType);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    private static JavaType constructParameterizedType(JavaType javaType, Class<?>... parameterClasses) {
+        if (Objects.isNull(javaType) && parameterClasses.length == 1) {
+            return MAPPER.getTypeFactory().constructType(parameterClasses[parameterClasses.length - 1]);
+        }
+        // 首次初始化
+        if (Objects.isNull(javaType)) {
+            JavaType type = MAPPER.getTypeFactory().constructType(parameterClasses[parameterClasses.length - 1]);
+            Class<?>[] outerClasses = new Class<?>[parameterClasses.length - 1];
+            System.arraycopy(parameterClasses, 0, outerClasses, 0, outerClasses.length);
+            return constructParameterizedType(type, outerClasses);
+        }
+
+        // 递归终止
+        if (parameterClasses.length == 1) {
+            return MAPPER.getTypeFactory()
+                .constructParametricType(parameterClasses[parameterClasses.length - 1], javaType);
+        }
+
+        // 循环构造
+        JavaType newType =
+            MAPPER.getTypeFactory().constructParametricType(parameterClasses[parameterClasses.length - 1], javaType);
+        Class<?>[] outerClasses = new Class<?>[parameterClasses.length - 1];
+        System.arraycopy(parameterClasses, 0, outerClasses, 0, outerClasses.length);
+        return constructParameterizedType(newType, outerClasses);
+    }
+
+    // -------------------------------------------------------------------------------------
+    // ----------------------             【集合相关】                  ----------------------
+    // -------------------------------------------------------------------------------------
+
+    /**
+     * 将json数组转换为指定泛型的List
+     *
+     * @param json           json数组
+     * @param elementClazzes 集合泛型
+     * @param <T>            普通实体类（不包括嵌套泛型的类型，比如CommonResult<T>这种）
+     * @return List集合
+     */
+    public static <T> List<T> str2List(String json, Class<T> elementClazzes) {
+        if (StringUtils.isBlank(json) || Objects.isNull(elementClazzes)) {
+            return Collections.emptyList();
+        }
+
+        try {
+            JavaType javaType = constructParametricType(ArrayList.class, elementClazzes);
+            return MAPPER.readValue(json, javaType);
+        } catch (Exception e) {
+            log.error("Parse String to List error : {}", e.getMessage(), e);
+            return Collections.emptyList();
+        }
+    }
+
+    /**
+     * 将json数组转换为指定泛型的Colllection集合(List、Set等)
+     *
+     * @param json            json数组
+     * @param collectionClazz 集合容器
+     * @param elementClazzes  集合泛型
+     * @param <T>             集合泛型
+     * @return Colllection集合
+     */
+    public static <T> T str2Collection(String json, Class<? extends Collection> collectionClazz,
+        Class<?>... elementClazzes) {
+        if (StringUtils.isBlank(json)) {
+            return null;
+        }
+
+        try {
+            JavaType javaType = constructParametricType(collectionClazz, elementClazzes);
+            return MAPPER.readValue(json, javaType);
+        } catch (Exception e) {
+            log.error("Parse String to Colllection error : {}", e.getMessage(), e);
+            return null;
+        }
+    }
+
+    /**
+     * 获取泛型的Collection Type(另请参考：TypeReference type = new TypeReference<List<T>>() {})
+     *
+     * @param collectionClazz 泛型的Collection
+     * @param elementClasses  元素类
+     * @return JavaType Java类型
+     */
+    public static JavaType constructParametricType(Class<?> collectionClazz, Class<?>... elementClasses) {
+        return MAPPER.getTypeFactory().constructParametricType(collectionClazz, elementClasses);
+    }
+
+    /**
+     * 构造 TypeReference 实例对象，不通用所以不推荐
+     *
+     * @param <T> 集合泛型
+     * @return TypeReference
+     */
+    public static <T> TypeReference<List<T>> getListTypeRef() {
+        return new TypeReference<List<T>>() {
+        };
+    }
+
+    // -------------------------------------------------------------------------------------
+    // ----------------------             【Map相关】                  ----------------------
+    // -------------------------------------------------------------------------------------
+
+    /**
+     * json转map
+     *
+     * @param map   map
+     * @param clazz 实体bean
+     * @param <T>   泛型
+     * @return 实体bean
+     */
+    public static <T> T map2Bean(Map<String, Object> map, Class<T> clazz) {
+        return MAPPER.convertValue(map, clazz);
+    }
+
+    public static <K, V> Map<K, V> str2HashMap(String json, Class<K> keyClazz, Class<V> valueClazz) {
+        return str2Map(json, HashMap.class, keyClazz, valueClazz);
+    }
+
+    /**
+     * 将json转换为指定泛型的Map集合(HashMap、TreeMap等)
+     *
+     * @param json       json
+     * @param mapClazz   Map类型
+     * @param keyClazz   键类型
+     * @param valueClazz 值类型
+     * @param <K>        键类型
+     * @param <V>        值类型
+     * @return Map集合
+     */
+    public static <K, V> Map<K, V> str2Map(String json, Class<? extends Map> mapClazz, Class<K> keyClazz,
+        Class<V> valueClazz) {
+        if (StringUtils.isBlank(json)) {
+            return Collections.emptyMap();
+        }
+
+        try {
+            JavaType javaType = getMapType(mapClazz, keyClazz, valueClazz);
+            return MAPPER.readValue(json, javaType);
+        } catch (Exception e) {
+            log.error("Parse String to Map error : {}", e.getMessage(), e);
+            return Collections.emptyMap();
+        }
+    }
+
+    /**
+     * 获取泛型的Map Type（另外：TypeReference<Map<K, V>> typeRef = new TypeReference<Map<K, V>>() {};）
+     *
+     * @param mapClazz   map容器
+     * @param keyClazz   键类型
+     * @param valueClazz 值类型
+     * @return map集合
+     */
+    public static JavaType getMapType(Class<? extends Map> mapClazz, Class<?> keyClazz, Class<?> valueClazz) {
+        return MAPPER.getTypeFactory().constructMapType(mapClazz, keyClazz, valueClazz);
+    }
+
+    // -------------------------------------------------------------------------------------
+    // ----------------------             【递归构造泛型】                  ----------------------
+    // -------------------------------------------------------------------------------------
+
+    /**
+     * 根据ParameterizedType构造JavaType(比如根据已知属性CommonResult<List<User>> property进行构造)
+     *
+     * @param genericTypePtz ParameterizedType
+     * @return JavaType
+     */
+    public static JavaType constructJavaType(ParameterizedType genericTypePtz) {
+        Type rawType = genericTypePtz.getRawType();
+        Type actualTypeArgument = genericTypePtz.getActualTypeArguments()[0];
+        if (actualTypeArgument instanceof ParameterizedType) {
+            JavaType javaType = constructJavaType((ParameterizedType)actualTypeArgument);
+            return MAPPER.getTypeFactory().constructParametricType((Class<?>)rawType, javaType);
+        }
+        return MAPPER.getTypeFactory().constructParametricType((Class<?>)rawType, (Class<?>)actualTypeArgument);
+
+    }
+
+    // -------------------------------------------------------------------------------------
+    // ----------------------             【代码封装整合】                  ----------------------
+    // -------------------------------------------------------------------------------------
+
+    /**
+     * 封装包含异常的转换方法，避免冗余代码
+     *
+     * @param parser 转换方法
+     * @param check  异常
+     * @param <T>    结果类型
+     * @return 转换结果
+     */
+    protected final <T> T tryParse(Callable<T> parser, Class<? extends Exception> check) {
+        try {
+            return parser.call();
+        } catch (Exception ex) {
+            log.error("Failed to try parse: error-[{}]", ex.getMessage(), ex);
+            if (check.isAssignableFrom(ex.getClass())) {
+                throw new RuntimeException("Cannot parse JSON", ex);
+            }
             return null;
         }
     }
--- cloud_common/src/test/java/com/lew/common/util/JacksonUtilTest.java
+++ cloud_common/src/test/java/com/lew/common/util/JacksonUtilTest.java
@@ -5,13 +5,18 @@
 package com.lew.common.util;
 
 import com.fasterxml.jackson.core.type.TypeReference;
+import com.lew.common.entity.CommonResult;
 import com.lew.common.entity.User;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.collections4.CollectionUtils;
+import org.apache.commons.collections4.MapUtils;
+import org.junit.Before;
 import org.junit.Test;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
 
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
@@ -22,15 +27,24 @@ import static org.junit.Assert.assertTrue;
  */
 @Slf4j
 public class JacksonUtilTest {
+    public static final String USER_JSON = "{\"name\":\"lew\",\"gender\":\"男\",\"age\":1}";
+
+    private static User user;
+
+    @Before
+    public void before() {
+        user = JacksonUtil.str2Obj(USER_JSON, User.class);
+    }
+
     @Test
     public void testJsonObjAndList() {
         User user1 = new User();
         user1.setAge(1);
         user1.setName("lew");
-        String userJsonstr = JacksonUtil.obj2String(user1);
+        String userJsonstr = JacksonUtil.obj2Str(user1);
         log.info("userJson: {}", userJsonstr);
 
-        User user2 = JacksonUtil.string2Obj(userJsonstr, User.class);
+        User user2 = JacksonUtil.str2Obj(userJsonstr, User.class);
         assertNotNull(user2);
 
         user2.setAge(2);
@@ -38,14 +52,65 @@ public class JacksonUtilTest {
         List<User> userList = new ArrayList<>();
         userList.add(user1);
         userList.add(user2);
-        String userListJson = JacksonUtil.obj2String(userList);
+        String userListJson = JacksonUtil.obj2Str(userList);
 
-        List<User> userListBean = JacksonUtil.string2Obj(userListJson, new TypeReference<List<User>>() {});
+        List<User> userListBean = JacksonUtil.str2Obj(userListJson, new TypeReference<List<User>>() {});
         assertTrue(CollectionUtils.isNotEmpty(userListBean));
         userListBean.forEach(user -> log.info("{}-{}", user.getAge(), user.getName()));
 
         List<User> userListBean2 = JacksonUtil.str2Collection(userListJson, List.class, User.class);
         assertTrue(CollectionUtils.isNotEmpty(userListBean2));
         userListBean2.forEach(user -> log.info("{}-{}", user.getAge(), user.getName()));
+
+        List<User> userListBean3 = JacksonUtil.str2List(userListJson, User.class);
+        assertTrue(CollectionUtils.isNotEmpty(userListBean3));
+        userListBean3.forEach(user -> log.info("{}-{}", user.getAge(), user.getName()));
+    }
+
+    @Test
+    public void testCommonResult() {
+        CommonResult<User> result1 = CommonResult.success(user);
+        CommonResult<User> result2 = CommonResult.forbidden(user);
+        CommonResult<User> result3 = CommonResult.unauthorized(user);
+        String resultsJson = JacksonUtil.obj2Str(Arrays.asList(result1, result2, result3));
+        assertNotNull(resultsJson);
+
+        List<CommonResult<User>> results = JacksonUtil.str2Obj(resultsJson, new TypeReference<List<CommonResult<User>>>(){});
+        log.info("CommonResult: {}", JacksonUtil.obj2Str(results));
+
+        List<CommonResult<User>> results2 = JacksonUtil.str2GenericObj(resultsJson, List.class, CommonResult.class,
+            User.class);
+        assertTrue(CollectionUtils.isNotEmpty(results2));
+    }
+
+    @Test
+    public void testListTypeRef() {
+        User user1 = JacksonUtil.str2Obj(USER_JSON, User.class);
+        User user2 = JacksonUtil.str2Obj(USER_JSON, User.class);
+        String usersJson = JacksonUtil.obj2Str(Arrays.asList(user1, user2));
+        assertNotNull(usersJson);
+
+        List<User> users = JacksonUtil.str2Obj(usersJson, JacksonUtil.<User>getListTypeRef());
+        log.info("users: {}", JacksonUtil.obj2Str(users));
+        assertTrue(CollectionUtils.isNotEmpty(users));
+    }
+
+    @Test
+    public void testStr2HashMap() {
+        Map<String, Object> userMap = JacksonUtil.str2HashMap(USER_JSON, String.class, Object.class);
+        log.info("userMap: {}", JacksonUtil.obj2Str(userMap));
+
+        assertTrue(MapUtils.isNotEmpty(userMap));
+        assertTrue(userMap.containsKey("name"));
+    }
+
+    @Test
+    public void testMap2Bean() {
+        Map<String, Object> userMap = JacksonUtil.str2HashMap(USER_JSON, String.class, Object.class);
+        log.info("userMap: {}", JacksonUtil.obj2Str(userMap));
+
+        User user = JacksonUtil.map2Bean(userMap, User.class);
+        log.info("user: {}", JacksonUtil.obj2Str(user));
+        assertNotNull(user);
     }
 }
