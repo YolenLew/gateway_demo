727ab1c5b8c5d9388f7da618224f7eee86cf38b4
yolenlew <yolenlew@163.com>
2023-11-18 18:08:35 +0800

    feat: RestTemplateConfig请求配置

--- cloud_gateway/src/main/java/com/lew/gateway/config/RestTemplateConfig.java
+++ cloud_gateway/src/main/java/com/lew/gateway/config/RestTemplateConfig.java
@@ -4,8 +4,13 @@
 
 package com.lew.gateway.config;
 
+import org.apache.http.client.HttpClient;
+import org.apache.http.config.SocketConfig;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.impl.conn.PoolingHttpClientConnectionManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 
 /**
@@ -17,6 +22,32 @@ public class RestTemplateConfig {
 
     @Bean
     public RestTemplate restTemplate() {
-        return new RestTemplate();
+        PoolingHttpClientConnectionManager connectionManager = new PoolingHttpClientConnectionManager();
+        connectionManager.setDefaultSocketConfig(SocketConfig.custom().setTcpNoDelay(true).build());
+        connectionManager.setDefaultMaxPerRoute(15);
+        connectionManager.setMaxTotal(30);
+        HttpClient httpClient = HttpClientBuilder.create().setConnectionManager(connectionManager).build();
+        HttpComponentsClientHttpRequestFactory factory = new HttpComponentsClientHttpRequestFactory(httpClient);
+        // 超时时间设置
+        factory.setReadTimeout(3000);
+        factory.setConnectTimeout(3000);
+        factory.setConnectionRequestTimeout(3000);
+        return new RestTemplate(factory);
     }
+
+    @Bean(value = "proxyRestTemplate")
+    public RestTemplate proxyRestTemplate() {
+        PoolingHttpClientConnectionManager connectionManager = new PoolingHttpClientConnectionManager();
+        connectionManager.setDefaultSocketConfig(SocketConfig.custom().setTcpNoDelay(true).build());
+        connectionManager.setDefaultMaxPerRoute(20);
+        connectionManager.setMaxTotal(30);
+        HttpClient httpClient = HttpClientBuilder.create().setConnectionManager(connectionManager).build();
+        HttpComponentsClientHttpRequestFactory factory = new HttpComponentsClientHttpRequestFactory(httpClient);
+        // 超时时间设置
+        factory.setReadTimeout(5000);
+        factory.setConnectTimeout(5000);
+        factory.setConnectionRequestTimeout(5000);
+        return new RestTemplate(factory);
+    }
+
 }
