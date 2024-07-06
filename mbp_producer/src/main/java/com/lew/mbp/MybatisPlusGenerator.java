60fbac26199240970829c9ea2a40593c611f2a4a
yolenlew <yolenlew@163.com>
2024-07-06 19:46:21 +0800

    feat:引入3.5.7版的MybatisPlusGenerator代码生成器

---
+++ mbp_producer/src/main/java/com/lew/mbp/MybatisPlusGenerator.java
@@ -0,0 +1,38 @@
+
+/*
+ * Copyright (c) 2024. This is my custom copyright information.2024-2024. All rights reserved.
+ */
+
+package com.lew.mbp;
+
+import com.baomidou.mybatisplus.generator.AutoGenerator;
+import com.lew.mbp.config.MybatisPlusConfig;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.util.Properties;
+
+/**
+ * @author Yolen
+ * @date 2024/7/6
+ */
+public class MybatisPlusGenerator {
+    private static final Logger LOGGER = LoggerFactory.getLogger(MybatisPlusGenerator.class);
+
+    public static void main(String[] args) {
+        // 代码生成器
+        Properties properties = new Properties();
+        try {
+            properties.load(MybatisPlusGenerator.class.getResourceAsStream("/mybatis-plus.properties"));
+        } catch (IOException e) {
+            LOGGER.error("failed to load `mybatis-plus.properties` file", e);
+        }
+        MybatisPlusConfig mybatisPlusConfig = new MybatisPlusConfig(properties);
+        AutoGenerator mpg = new AutoGenerator(mybatisPlusConfig.dataSourceConfig().build());
+        mpg.global(mybatisPlusConfig.globalConfig());
+        mpg.packageInfo(mybatisPlusConfig.packageConfig());
+        mpg.strategy(mybatisPlusConfig.strategyConfig());
+        mpg.execute();
+    }
+}
